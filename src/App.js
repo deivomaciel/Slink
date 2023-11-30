64a056715d6cf40fdaa079478cf2b1d1e886d982
(Deivide <(deividemaciel2@gmail.com)>
2023-11-30 17:46:32 -0300

    fix: Correção de um erro do Google Analytics

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from "react";
 import { Provider } from "react-redux"
 import store from "./Components/store";
 import AppRoutes from "./routes/AppRoutes";
-import Analytics from "./Components/Analytics/Analytics";
 
 function App() {
   return (
     <div className="App">
       <Provider store={store}>
         <AppRoutes />
-        <Analytics />
       </Provider>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import ReactGA from 'react-ga4'
+
+ReactGA.initialize(process.env.REACT_APP_ANALYTICS_ID)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
